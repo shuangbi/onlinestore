40943840b15ecab3798f458efb08054aaa90776f
unknown <u0153435@U0153435-TPL-A.ten.thomsonreuters.com>
2013-10-28 17:30:58 +0800

    Add home controller with 'index contact about'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Onlinestore::Application.routes.draw do
+  get "home/index"
+
+  get "home/about"
+
+  get "home/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Onlinestore::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
